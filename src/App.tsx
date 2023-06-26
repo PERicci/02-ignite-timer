8371fcad4696f5716f6548f6737f20c1c3b165f5
Pedro Ricciardi <riccip@outlook.com>
2023-06-26 10:28:51 +0100

    styled components, global style and router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import { useState } from "react";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
-  return;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
